8375e2fd0c31bf6aff32e4d308eda4c0a7779d8e
Nahabwe Edwin <nahabwe.edwin12@gmail.com>
2022-03-20 16:50:59 +0300

    Create Project Release

---
+++ appexecutors/src/main/java/ug/musicmeetscode/appexecutors/AppExecutors.kt
@@ -0,0 +1,52 @@
+package ug.musicmeetscode.appexecutors
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+
+class AppExecutors private constructor(
+    private val diskIO: Executor,
+    private val networkIO: Executor,
+    private val mainThread: Executor
+) {
+    fun diskIO(): Executor {
+        return diskIO
+    }
+
+    fun mainThread(): Executor {
+        return mainThread
+    }
+
+    fun networkIO(): Executor {
+        return networkIO
+    }
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+
+    companion object {
+        private val LOCK = Any()
+
+        @JvmStatic
+        var instance: AppExecutors? = null
+            get() {
+                if (field == null) {
+                    synchronized(LOCK) {
+                        field = AppExecutors(
+                            Executors.newSingleThreadExecutor(),
+                            Executors.newFixedThreadPool(3),
+                            MainThreadExecutor()
+                        )
+                    }
+                }
+                return field
+            }
+            private set
+    }
+}
